c7f4317849d6e68aa26b61b65545775672c696f0
Bùi Đức Tài <69392382+BUIDUCTAI2105@users.noreply.github.com>
2020-08-15 12:08:03 +0700

    add compare the triplets

--- Compare the triplets.cs
+++ Compare the triplets.cs
@@ -46,16 +46,20 @@ class Solution
     }
 }
 
-Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+//Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
 
-The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+//The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
 
-The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+//The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
 
-If a[i] > b[i], then Alice is awarded 1 point.
-If a[i] < b[i], then Bob is awarded 1 point.
-If a[i] = b[i], then neither person receives a point.
-Comparison points is the total points a person earned.
+//If a[i] > b[i], then Alice is awarded 1 point.
+//If a[i] < b[i], then Bob is awarded 1 point.
+//If a[i] = b[i], then neither person receives a point.
+//Comparison points is the total points a person earned.
 
-đưa vào số điểm của Alice
-đưa vào số điểm của Bob
+//đưa vào số điểm của Alice a[i]
+//đưa vào số điểm của Bob  b[i]
+// nếu a[i] > b[i] thì Alice nhận đc 1 điểm
+//và  nếu a[i] < b[i] thì Bob nhận đc 1 điểm
+
+//cuối cùng trả về kq so sánh điểm của Alice và Bob
