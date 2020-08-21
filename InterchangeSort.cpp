5140f35122019864f04fac1b208c8785a5c739e1
Bùi Đức Tài <69392382+BUIDUCTAI2105@users.noreply.github.com>
2020-08-21 10:06:46 +0700

    add interchangeSort

--- InterchangeSort.cpp
+++ InterchangeSort.cpp
@@ -1,7 +1,8 @@
-// Interchange_Sort.cpp : Defines the entry point for the console application.
+// ConsoleApplication12.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
+
 void nhapmang(int a[], int &n)
 {
 	printf("nhap so phan tu mang: ");
@@ -19,30 +20,26 @@ void xuatmang(int a[], int n)
 	printf("%4d", a[i]);	
 	}
 }
-void InterchangeSort(int a[], int n)
+void InterchangeSort(int *a, int n)
 {
-	for (int i = 0; i < n; i++)
-	{
-	for (int j = i +1; j < n; j++)
-	{
-	if(a[i] > a[j])
-		swap(a[i], a[j]);
-	}
-	}
-}
-void swap (int &a, int &b)
-{
-	int x = a;
-	a = b;
-	b = x;
+   for (int i = 0; i < n-1; i++)
+      for (int j = i+1; j < n; j++)
+         if (a[i] > a[j])
+         {
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+         }
 }
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int A[100];
-	nhapmang(A , n);
-	InterchangeSort(A, n);
-	xuatmang(A , n);
+	int a[100];
+	int n;
+	nhapmang(a, n);
+	InterchangeSort(a, n);
+	xuatmang(a, n);
 	getchar();
 	return 0;
 }
+
 
