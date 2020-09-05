45fa059b8f7ed3a5e8bc0d6fabda7723d4f5d21f
Bùi Đức Tài <69392382+BUIDUCTAI2105@users.noreply.github.com>
2020-09-05 13:25:51 +0700

    add C++ Codelearn

---
+++ baihocC++.cpp
@@ -0,0 +1,18 @@
+ //B?n hãy vi?t chuong trình t?o ra 2 bi?n ki?u s? nguyên a và b. Sau dó nh?p giá tr? cho a và b t? bàn phím và hi?n th? ra màn hình:
+
+a + b = {P}
+
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << "a + b = " << a + b;
+	return 0;
+}
+
+
+
---
+++ baihocC++_2.cpp
@@ -0,0 +1,13 @@
+// B?n hãy vi?t chuong trình nh?p t? bàn phím chi?u dài và chi?u r?ng c?a m?t hình ch? nh?t (chi?u dài và chi?u r?ng c?a hình ch? nh?t này là m?t s? nguyên) sau dó hi?n th? ra màn hình:
+
+Area = {P}
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << "Area = " << a * b;
+	return 0;
+}
