a4c0b8345f435fd335b161b4382647595e59ec80
Bùi Đức Tài <69392382+BUIDUCTAI2105@users.noreply.github.com>
2020-09-08 20:37:29 +0700

    add matran thay khang

---
+++ MATRAN_THAYKHANG.1.cpp
@@ -0,0 +1,50 @@
+// bai hoc ma tran thay Khang
+
+// dinh nghia ham gia tri chan dau tien trong ma tran cac so nguyen
+
+#include<iostream>
+
+using namespace std;
+void NhapMaTran(int a[][100], int m, int n)
+{
+   for(int i = 0; i < m; i++)
+      for(int j = 0; j < n; j++)
+      {
+         printf("A[%d][%d] = ", i, j);
+         scanf("%d", &a[i][j]);
+      }
+}
+ 
+void XuatMaTran(int a[][100], int m, int n)
+{
+   for(int i = 0; i < m; i++)
+   {
+      for(int j = 0; j < n; j++)
+         printf("%d\t", a[i][j]);
+      printf("\n");
+   }
+}
+
+int ChanDau (int a[][100], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			if (a[i][j] % 2 ==0)
+				return a[i][j];
+	
+	return -1;			
+}
+
+int main()
+{
+    int a[100][100];
+    int m,n;
+    printf("nhap so hang n = "); scanf("%d",&n);
+    printf("nhap so cot m = "); scanf("%d",&m);
+    printf("nhap vao ma tran:\n");
+    NhapMaTran(a, m , n);
+    XuatMaTran(a, m, n);
+    
+    ChanDau (a, m, n);
+     
+	getchar();
