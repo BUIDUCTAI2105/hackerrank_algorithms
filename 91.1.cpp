aa11b40ea0176f6fc424b1dc344de8701e0663f8
Bùi Đức Tài <69392382+BUIDUCTAI2105@users.noreply.github.com>
2020-09-25 19:49:24 +0700

    add BT sửa lại 91

---
+++ 91.1.cpp
@@ -0,0 +1,17 @@
+// In tat ca các so nguyên duong le nho hon 100
+
+#include<stdio.h>
+
+int main()
+{
+		int i = 1;
+		while(i<100)
+		{
+
+			printf("%5d", i);
+			i = i+2;
+
+		}
+		getchar();
+		return 0;
+}
